f3759543d3aed538ff5bcddb541c8bdf55d11262
Muhammad Ahsan Zahoor <109317164+codewithahsanzahoor@users.noreply.github.com>
2024-11-19 15:56:48 +0500

    dashboard layout setup and home page create

---
+++ src/layouts/DashboardLayout.tsx
@@ -0,0 +1,94 @@
+import { Link, Outlet } from "react-router-dom";
+
+function DashboardLayout() {
+	return (
+		<div>
+			<div id="navbar">
+				<div className="navbar bg-base-100">
+					<div className="flex-1">
+						<a className="btn btn-ghost text-xl">daisyUI</a>
+					</div>
+					<div className="flex-none gap-2">
+						<div className="form-control">
+							<input
+								type="text"
+								placeholder="Search"
+								className="input input-bordered w-24 md:w-auto"
+							/>
+						</div>
+						<div className="dropdown dropdown-end">
+							<div
+								tabIndex={0}
+								role="button"
+								className="btn btn-ghost btn-circle avatar"
+							>
+								<div className="w-10 rounded-full">
+									<img
+										alt="Tailwind CSS Navbar component"
+										src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+									/>
+								</div>
+							</div>
+							<ul
+								tabIndex={0}
+								className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+							>
+								<li>
+									<a className="justify-between">
+										Profile
+										<span className="badge">New</span>
+									</a>
+								</li>
+								<li>
+									<a>Settings</a>
+								</li>
+								<li>
+									<a>Logout</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div id="dashboard" className="grid grid-cols-12 gap-1">
+				{/* //! sidebar */}
+				<div className="hidden sm:block sm:col-span-3  h-[calc(100vh-0.2rem)] bg-base-200 ">
+					<h1 className="text-2xl font-bold text-center my-3">Sidebar</h1>
+					<div className="divider my-0 mx-0"></div>
+					<ul className="menu bg-base-200 w-full gap-5 mt-[15px]">
+						<li>
+							<a>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									className="h-5 w-5"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke="currentColor"
+								>
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										strokeWidth="2"
+										d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+									/>
+								</svg>
+								<Link to="/dashboard/home">Home</Link>
+							</a>
+						</li>
+						<li>
+							<a>
+								<i className="ri-git-repository-fill"></i>{" "}
+								<Link to="/dashboard/books">Books</Link>
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div className="col-span-12 sm:col-span-9  h-[calc(100vh-5rem)]">
+					<Outlet />
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default DashboardLayout;
--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import DashboardLayout from "./layouts/DashboardLayout";
+import TablePage from "./pages/TablePage";
+import BooksPage from "./pages/BooksPage";
 
 export const router = createBrowserRouter([
 	{
@@ -10,6 +13,20 @@ export const router = createBrowserRouter([
 	{
 		path: "/register",
 		element: <RegisterPage />,
+	},
+	{
+		path: "dashboard",
+		element: <DashboardLayout />,
+		children: [
+			{
+				path: "home",
+				element: <TablePage />,
+			},
+			{
+				path: "books",
+				element: <BooksPage />,
+			},
+		],
 	},
 	{
 		path: "/",
